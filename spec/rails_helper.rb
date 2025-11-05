8a77dbffc0d704cc81ff7bc7ed0cab549a49847f
松村紘一朗 <matsumurakoichiro@matsumurakouichiakiranoMac-mini.local>
2025-11-05 10:27:59 +0900

    CI再修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,12 +27,12 @@ require 'rspec/rails'
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
-begin
-  # ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
+# begin
+  ActiveRecord::Migration.maintain_test_schema!
+# rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
-end
-RSpec.configure do |config|
+# end
+# RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
@@ -67,4 +67,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-end
+# end
