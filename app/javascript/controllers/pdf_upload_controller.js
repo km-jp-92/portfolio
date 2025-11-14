8717cfbb2c605e4f846be5a21ac3ddc155ded8a8
松村紘一朗 <matsumurakoichiro@matsumurakouichiakiranoMac-mini.local>
2025-11-14 12:09:28 +0900

    PDFアップロードページのレイアウト修正

--- app/javascript/controllers/pdf_upload_controller.js
+++ app/javascript/controllers/pdf_upload_controller.js
@@ -1,44 +1,30 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="pdf-upload"
 export default class extends Controller {
-  static targets = ["form"]
+  static targets = ["dropzone", "fileInput"]
 
   connect() {
-    this.dropArea = this.formTarget.querySelector("label")
-    this.fileInput = this.formTarget.querySelector('input[type="file"]')
+    this.dropzoneTarget.addEventListener("dragover", this.handleDragOver)
+    this.dropzoneTarget.addEventListener("drop", this.handleDrop)
+  }
 
-    // ドロップ時の処理
-    this.formTarget.addEventListener("dragover", this.handleDragOver.bind(this))
-    this.formTarget.addEventListener("dragleave", this.handleDragLeave.bind(this))
-    this.formTarget.addEventListener("drop", this.handleDrop.bind(this))
+  disconnect() {
+    this.dropzoneTarget.removeEventListener("dragover", this.handleDragOver)
+    this.dropzoneTarget.removeEventListener("drop", this.handleDrop)
   }
 
   upload() {
-    this.formTarget.requestSubmit()
+    this.fileInputTarget.form.requestSubmit()
   }
 
-  handleDragOver(event) {
-    event.preventDefault()
-    event.stopPropagation()
-    this.dropArea.classList.add("bg-blue-100") // 視覚的に反応
-  }
+  handleDragOver = (event) => event.preventDefault()
 
-  handleDragLeave(event) {
+  handleDrop = (event) => {
     event.preventDefault()
-    event.stopPropagation()
-    this.dropArea.classList.remove("bg-blue-100")
-  }
-
-  handleDrop(event) {
-    event.preventDefault()
-    event.stopPropagation()
-    this.dropArea.classList.remove("bg-blue-100")
 
     const files = event.dataTransfer.files
     if (files.length > 0) {
-      // input[type="file"] にドラッグしたファイルをセット
-      this.fileInput.files = files
+      this.fileInputTarget.files = files
       this.upload()
     }
   }
