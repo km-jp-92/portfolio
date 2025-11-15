8da18a42334c63d42d3c74629ec1ab70e8a455fc
松村紘一朗 <matsumurakoichiro@matsumurakouichiakiranoMac-mini.local>
2025-11-15 13:32:27 +0900

    不要なカラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_14_110509) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_15_042645) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,7 +51,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_14_110509) do
     t.datetime "reset_token_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "access_expires_at"
     t.string "upload_token"
     t.string "view_token"
     t.datetime "upload_token_expires_at"
