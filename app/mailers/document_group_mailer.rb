00f3697d528e3d10102c7d9cb21c305af3f8e00b
松村紘一朗 <matsumurakoichiro@matsumurakouichiakiranoMac-mini.local>
2025-11-15 16:58:29 +0900

    Lint修正

--- app/mailers/document_group_mailer.rb
+++ app/mailers/document_group_mailer.rb
@@ -13,7 +13,7 @@ class DocumentGroupMailer < ApplicationMailer
     qrcode = RQRCode::QRCode.new(@viewer_url)
     svg = qrcode.as_svg(module_size: 6, standalone: true)
     qr_data = svg
-    attachments.inline["qr_code.svg"] = { mime_type: 'image/svg+xml', content: qr_data }
+    attachments.inline["qr_code.svg"] = { mime_type: "image/svg+xml", content: qr_data }
 
     mail(to: @document_group.email, subject: "PDF同期サービス：パスワード設定とPDFリンクのご案内")
   end
