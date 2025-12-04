43730ca54e8034c7b6ca7c311e6fa53ff9e724e4
松村紘一朗 <matsumurakoichiro@matsumurakouichiakiranoMac-mini.local>
2025-12-05 00:15:03 +0900

    テストコード

--- spec/requests/document_groups_spec.rb
+++ spec/requests/document_groups_spec.rb
@@ -1,17 +1,19 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "DocumentGroups", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/document_groups/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
+# RSpec.describe "DocumentGroups", type: :request do
+#   pending "not yet implemented"
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/document_groups/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-end
+#   describe "GET /new" do
+#     it "returns http success" do
+#       get "/document_groups/new"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+
+#   describe "GET /create" do
+#     it "returns http success" do
+#       get "/document_groups/create"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+# end
